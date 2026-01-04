06cf31c4c3e4334bb6be8fcbdc60203a6b840d67
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-01-04 21:51:06 +0300

    Product Controller bitirildi.

---
+++ SignalRApi/Controllers/ProductController.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.ProductDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        private readonly IMapper _mapper;
+
+        public ProductController(IProductService productService, IMapper mapper)
+        {
+            _productService = productService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult ProductList()
+        {
+            var result = _mapper.Map<List<ResultProductDto>>(_productService.TGetListAll());
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(CreateProductDto dto)
+        {
+            var value = _mapper.Map<Product>(dto);
+            _productService.TAdd(value);
+            return Ok("Ürün eklendi");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteProduct(int id)
+        {
+            var value = _productService.TGetById(id);
+            _productService.TDelete(value);
+            return Ok("Ürün Silindi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateProduct(UpdateProductDto dto)
+        {
+            var value = _mapper.Map<Product>(dto);
+            _productService.TUpdate(value);
+            return Ok("Ürün güncellendi");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            var value = _productService.TGetById(id);
+            var result = _mapper.Map<GetProductDto>(value);
+            return Ok(result);
+        }
+    }
+}
