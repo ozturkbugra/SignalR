0595eba1823d9204d05dec70a93fd8d0030b63b4
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-01-04 22:01:09 +0300

    Testimonial Controller bitirildi.

---
+++ SignalRApi/Controllers/TestimonialController.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.TestimonialDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialController : ControllerBase
+    {
+        private readonly ITestimonialService _testimonialService;
+        private readonly IMapper _mapper;
+
+        public TestimonialController(ITestimonialService testimonialService, IMapper mapper)
+        {
+            _testimonialService = testimonialService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult TestimonialList()
+        {
+            var result = _mapper.Map<List<ResultTestimonialDto>>(_testimonialService.TGetListAll());
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult CreateTestimonial(CreateTestimonialDto dto)
+        {
+            var value = _mapper.Map<Testimonial>(dto);
+            _testimonialService.TAdd(value);
+            return Ok("Müşteri Yorum Bilgisi eklendi");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteTestimonial(int id)
+        {
+            var value = _testimonialService.TGetById(id);
+            _testimonialService.TDelete(value);
+            return Ok("Müşteri Yorum Bilgisi Silindi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateTestimonial(UpdateTestimonialDto dto)
+        {
+            var value = _mapper.Map<Testimonial>(dto);
+            _testimonialService.TUpdate(value);
+            return Ok("Müşteri Yorum Bilgisi güncellendi");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetTestimonial(int id)
+        {
+            var value = _testimonialService.TGetById(id);
+            var result = _mapper.Map<GetTestimonialDto>(value);
+            return Ok(result);
+        }
+    }
+}
