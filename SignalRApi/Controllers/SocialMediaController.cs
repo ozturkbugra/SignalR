203ba4853c9d3fd789b729d06a760df31b861b72
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-01-04 21:56:32 +0300

    SocialMedia Controller bitirildi.

---
+++ SignalRApi/Controllers/SocialMediaController.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.SocialMediaDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SocialMediaController : ControllerBase
+    {
+        private readonly ISocialMediaService _socialMediaService;
+        private readonly IMapper _mapper;
+
+        public SocialMediaController(ISocialMediaService socialMediaService, IMapper mapper)
+        {
+            _socialMediaService = socialMediaService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult SocialMediaList()
+        {
+            var result = _mapper.Map<List<ResultSocialMediaDto>>(_socialMediaService.TGetListAll());
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult CreateSocialMedia(CreateSocialMediaDto dto)
+        {
+            var value = _mapper.Map<SocialMedia>(dto);
+            _socialMediaService.TAdd(value);
+            return Ok("Sosyal Medya eklendi");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteSocialMedia(int id)
+        {
+            var value = _socialMediaService.TGetById(id);
+            _socialMediaService.TDelete(value);
+            return Ok("Sosyal Medya Silindi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateSocialMedia(UpdateSocialMediaDto dto)
+        {
+            var value = _mapper.Map<SocialMedia>(dto);
+            _socialMediaService.TUpdate(value);
+            return Ok("Sosyal Medya güncellendi");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSocialMedia(int id)
+        {
+            var value = _socialMediaService.TGetById(id);
+            var result = _mapper.Map<GetSocialMediaDto>(value);
+            return Ok(result);
+        }
+    }
+}
