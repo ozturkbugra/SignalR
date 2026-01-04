29160e34c5d9695ac584802898f61218b3ae21b6
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-01-04 19:11:12 +0300

    Manager Sınıfları Yazıldı Yani Service Interfacelerinin Classları yazıldı.

---
+++ SignalR.BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,46 @@
+﻿using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        private readonly ICategoryDal _categorDal;
+
+        public CategoryManager(ICategoryDal categorDal)
+        {
+            _categorDal = categorDal;
+        }
+
+        public void TAdd(Category entity)
+        {
+            _categorDal.Add(entity);
+        }
+
+        public void TDelete(Category entity)
+        {
+            _categorDal.Delete(entity);
+        }
+
+        public Category TGetById(int id)
+        {
+            return _categorDal.GetById(id);
+        }
+
+        public List<Category> TGetListAll()
+        {
+           return _categorDal.GetListAll();
+        }
+
+        public void TUpdate(Category entity)
+        {
+            _categorDal.Update(entity);
+        }
+    }
+}
