5f8c5e252fbec826bffbcf06a2d8123982093768
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-01-04 16:07:09 +0300

    Entityler Yazıldı.

---
+++ SignalR.EntityLayer/Entities/Booking.cs
@@ -0,0 +1,12 @@
+﻿namespace SignalRApi.DAL.Entities
+{
+    public class Booking
+    {
+        public int BookingID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int PersonCount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
