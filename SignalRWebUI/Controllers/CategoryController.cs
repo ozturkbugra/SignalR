5d9cc9a44b8a52793f199259683a56f020392b73
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-01-07 00:23:09 +0300

    Kategori Ekleme İşlemi Yapıldı.

--- SignalRWebUI/Controllers/CategoryController.cs
+++ SignalRWebUI/Controllers/CategoryController.cs
@@ -26,5 +26,24 @@ namespace SignalRWebUI.Controllers
             }
             return View();
         }
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            createCategoryDto.CategoryStatus = true;
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCategoryDto);
+            StringContent stringContent = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7171/api/Category", stringContent);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
