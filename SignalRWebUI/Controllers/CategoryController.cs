1b7c5ee68c0da5e758d6d3e5f66bfe6d71c9796a
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-01-07 00:05:18 +0300

    Category tablosu Api'den çekildi ve View'da listelendi.

--- SignalRWebUI/Controllers/CategoryController.cs
+++ SignalRWebUI/Controllers/CategoryController.cs
@@ -1,11 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.CategoryDtos;
+using System.Threading.Tasks;
 
 namespace SignalRWebUI.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CategoryController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responeMessage = await client.GetAsync("https://localhost:7171/api/Category");
+            if(responeMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responeMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
