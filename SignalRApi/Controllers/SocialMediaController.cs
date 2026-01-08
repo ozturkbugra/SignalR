8ee6e22f9531d681a0227970f78e05e6e3453e40
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-01-08 21:52:03 +0300

    SocialMedia CRUD işlemleri yapıldı.

--- SignalRApi/Controllers/SocialMediaController.cs
+++ SignalRApi/Controllers/SocialMediaController.cs
@@ -35,7 +35,7 @@ namespace SignalRApi.Controllers
             return Ok("Sosyal Medya eklendi");
         }
 
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public IActionResult DeleteSocialMedia(int id)
         {
             var value = _socialMediaService.TGetById(id);
---
+++ SignalRWebUI/Controllers/SocialMediaController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SignalRWebUI.Dtos.SocialMediaDtos;
+
+namespace SignalRWebUI.Controllers
+{
+    public class SocialMediaController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public SocialMediaController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7171/api/SocialMedia");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = responseMessage.Content.ReadAsStringAsync().Result;
+                var values = Newtonsoft.Json.JsonConvert.DeserializeObject<List<ResultSocialMediaDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        public IActionResult CreateSocialMedia()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSocialMedia(CreateSocialMediaDto createSocialMediaDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = Newtonsoft.Json.JsonConvert.SerializeObject(createSocialMediaDto);
+            StringContent stringContent = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7171/api/SocialMedia", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteSocialMedia(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7171/api/SocialMedia/{id}");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> UpdateSocialMedia(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7171/api/SocialMedia/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = Newtonsoft.Json.JsonConvert.DeserializeObject<GetSocialMediaDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateSocialMedia(UpdateSocialMediaDto updateSocialMediaDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = Newtonsoft.Json.JsonConvert.SerializeObject(updateSocialMediaDto);
+            StringContent stringContent = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7171/api/SocialMedia", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
