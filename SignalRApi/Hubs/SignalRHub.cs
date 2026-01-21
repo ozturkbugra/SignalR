9f763ef7261496b2e8f6bfe38076b7779b1bccc7
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-01-22 00:27:10 +0300

    2 farklı istatistiği aynı anda çektik.

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -1,21 +1,31 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalR.BusinessLayer.Abstract;
 using SignalR.DataAccessLayer.Concrete;
 
 namespace SignalRApi.Hubs
 {
     public class SignalRHub : Hub
     {
-        private readonly SignalRContext _context;
 
-        public SignalRHub(SignalRContext context)
+        private readonly ICategoryService _categoryService;
+        private readonly IProductService _productService;
+
+        public SignalRHub(ICategoryService categoryService, IProductService productService)
         {
-            _context = context;
+            _categoryService = categoryService;
+            _productService = productService;
         }
 
         public async Task SendCategoryCount()
         {
-            var count = _context.Categories.Count();
+            var count = _categoryService.TCategoryCount();
             await Clients.All.SendAsync("ReceiveCategoryCount", count);
+        }
+
+        public async Task SendProductCount()
+        {
+            var count = _productService.TProductCount();
+            await Clients.All.SendAsync("ReceiveProductCount", count);
         }
     }
 }
