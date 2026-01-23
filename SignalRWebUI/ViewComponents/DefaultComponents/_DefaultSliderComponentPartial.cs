05f13b61f9252bfab6f18a5ca085e588c87700eb
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-01-23 20:24:47 +0300

    Feature Api Consume Edildi.

--- SignalRWebUI/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.FeatureDtos;
 
 namespace SignalRWebUI.ViewComponents.DefaultComponents
 {
     public class _DefaultSliderComponentPartial: ViewComponent
     {
-        public IViewComponentResult Invoke()
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultSliderComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responeMessage = await client.GetAsync("https://localhost:7171/api/Feature");
+            if (responeMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responeMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
