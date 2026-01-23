fe48e2d0f57f1d6858a1c34c8ce2f25e38ed7cba
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-01-23 20:32:51 +0300

    İndirimler Apiden Çekildi.

--- SignalRWebUI/ViewComponents/DefaultComponents/_DefaultOfferComponentPartial.cs
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultOfferComponentPartial.cs
@@ -1,11 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.DiscountDtos;
 
 namespace SignalRWebUI.ViewComponents.DefaultComponents
 {
     public class _DefaultOfferComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultOfferComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responeMessage = await client.GetAsync("https://localhost:7171/api/Discount");
+            if (responeMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responeMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultDiscountDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
