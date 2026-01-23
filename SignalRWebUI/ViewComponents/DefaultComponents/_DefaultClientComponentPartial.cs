772776aea4bc460d23483077a09d1b465f200a91
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-01-23 19:57:40 +0300

    Default Sayfası Tamamlandı.

---
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultClientComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.ViewComponents.DefaultComponents
+{
+    public class _DefaultClientComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
