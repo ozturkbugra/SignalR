059bb4e1ae0b0bbce9a838d6d2a61c6313ca564e
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-01-24 00:55:48 +0300

    Sepet sayfası yapılmaya başlandı.

---
+++ SignalRWebUI/Controllers/BasketController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.AboutDtos;
+using SignalRWebUI.Dtos.BasketDtos;
+
+namespace SignalRWebUI.Controllers
+{
+    public class BasketController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BasketController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responeMessage = await client.GetAsync("https://localhost:7171/api/Basket/GetBasketByMenuTableNumber");
+            if (responeMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responeMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBasketDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
