e20163921bedddcd0620b206e6181ac6ad5e6603
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-01-25 13:03:57 +0300

    Rezervasyon Yap Kısmı Yapıldı.

--- SignalRWebUI/Controllers/BookATableController.cs
+++ SignalRWebUI/Controllers/BookATableController.cs
@@ -1,11 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.AboutDtos;
+using SignalRWebUI.Dtos.BookingDtos;
+using System.Net.Http;
 
 namespace SignalRWebUI.Controllers
 {
     public class BookATableController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BookATableController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBooking(CreateBookingDto createBookingDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createBookingDto);
+            StringContent stringContent = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7171/api/Booking", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
