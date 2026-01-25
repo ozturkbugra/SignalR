5465f49dcbf5e6f05b0f9c442a7a62ca1ba88380
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-01-25 14:57:13 +0300

    Notification api tamamen bitirildi.

--- SignalRApi/Controllers/NotificationController.cs
+++ SignalRApi/Controllers/NotificationController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.AboutDto;
+using SignalR.DtoLayer.NotificationDto;
+using SignalR.EntityLayer.Entities;
 
 namespace SignalRApi.Controllers
 {
@@ -8,11 +12,14 @@ namespace SignalRApi.Controllers
     [ApiController]
     public class NotificationController : ControllerBase
     {
-        private readonly INotificationService _notificationService;
+        private readonly INotificationService _notificationService; 
+        private readonly IMapper _mapper;
 
-        public NotificationController(INotificationService notificationService)
+
+        public NotificationController(INotificationService notificationService, IMapper mapper)
         {
             _notificationService = notificationService;
+            _mapper = mapper;
         }
 
         [HttpGet]
@@ -31,6 +38,38 @@ namespace SignalRApi.Controllers
             return Ok(_notificationService.TGetAllNotificationByFalse());
         }
 
+        [HttpPost]
+        public IActionResult CreateNotification(CreateNotificationDto createNotificationDto)
+        {
+            var value = _mapper.Map<Notification>(createNotificationDto);
+            _notificationService.TAdd(value);
+            return Ok("Bildirim eklendi");
+        }
+        
+        [HttpDelete("{id}")]
+        public IActionResult DeleteNotification(int id)
+        {
+            var value = _notificationService.TGetById(id);
+            _notificationService.TDelete(value);
+            return Ok("Bildirim Silindi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateAbout(UpdateNotificationDto updateNotificationDto)
+        {
+            var value = _mapper.Map<Notification>(updateNotificationDto);
+            _notificationService.TUpdate(value);
+            return Ok("Bildirim güncellendi");
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetAbout(int id)
+        {
+            var value = _notificationService.TGetById(id);
+            var result = _mapper.Map<GetNotificationDto>(value);
+            return Ok(result);
+        }
 
     }
 }
