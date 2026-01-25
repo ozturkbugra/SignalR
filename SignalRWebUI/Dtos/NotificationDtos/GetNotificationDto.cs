92a17fb313231c84ba5971b2e7519c05aed69bfc
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-01-25 15:19:24 +0300

    Admin kısmında bildirim sayfası ayrıca yapıldı

---
+++ SignalRWebUI/Dtos/NotificationDtos/GetNotificationDto.cs
@@ -0,0 +1,14 @@
+﻿namespace SignalRWebUI.Dtos.NotificationDtos
+{
+    public class GetNotificationDto
+    {
+        public int NotificationID { get; set; }
+        public string Type { get; set; }
+        public string Icon { get; set; }
+        public string Description { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
