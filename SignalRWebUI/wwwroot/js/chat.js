a8da247ea4a3014f0f2de2db543a3b55a72041a3
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-01-25 17:29:27 +0300

    Mesajlaşma işi yapıldı ama veri tabanına kaydedilmeden.

--- SignalRWebUI/wwwroot/js/chat.js
+++ SignalRWebUI/wwwroot/js/chat.js
@@ -1,36 +1,56 @@
+"use strict";
+
 var connection = new signalR.HubConnectionBuilder()
     .withUrl("https://localhost:7171/SignalRHub")
     .build();
 
-document.getElementById("sendButton").disabled = true;
+// Başta butonu kapat
+var sendButton = document.getElementById("sendButton");
+sendButton.disabled = true;
 
+// Mesaj alma
 connection.on("ReceiveMessage", function (user, message) {
-    var currentTime = new Date(),
-    var currentHour = currentTime.getHours();
-    var currentMinute = currentTime.getMinutes();
+
+    var now = new Date();
+    var hour = now.getHours().toString().padStart(2, "0");
+    var minute = now.getMinutes().toString().padStart(2, "0");
 
     var li = document.createElement("li");
+
     var span = document.createElement("span");
     span.style.fontWeight = "bold";
     span.textContent = user;
+
     li.appendChild(span);
-    li.innerHTML += `: ${message} - ${currentHour}: ${currentMinute}`;
+    li.appendChild(
+        document.createTextNode(`: ${message} - ${hour}:${minute}`)
+    );
+
     document.getElementById("messageList").appendChild(li);
 });
 
-
-connection.start().then(function () {
-    document.getElementById("sendButton").disabled = false;
-}).catch(function (err) {
-    return console.error(err.toString());
-});
-
-document.getElementById("sendButton").addEventListener("click", function (event) {
-    var user = document.getElementById("userInput").value;
-    var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
-        return console.error(err.toString());
+// Bağlantıyı başlat
+connection.start()
+    .then(function () {
+        sendButton.disabled = false;
+    })
+    .catch(function (err) {
+        console.error(err.toString());
     });
+
+// Mesaj gönderme
+sendButton.addEventListener("click", function (event) {
     event.preventDefault();
 
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+
+    if (user === "" || message === "") return;
+
+    connection.invoke("SendMessage", user, message)
+        .catch(function (err) {
+            console.error(err.toString());
+        });
+
+    document.getElementById("messageInput").value = "";
 });
