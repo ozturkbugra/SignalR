743a9bc3af2546a7ff96ed512cdfe333e8ff46d2
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-01-25 22:10:27 +0300

    Login kısmı yapıldı.

---
+++ SignalRWebUI/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.EntityLayer.Entities;
+using SignalRWebUI.Dtos.IdentityDtos;
+
+namespace SignalRWebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginDto loginDto)
+        {
+            var result = await _signInManager.PasswordSignInAsync(
+                loginDto.Username,
+                loginDto.Password,
+                false,
+                false
+            );
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Category");
+            }
+
+            ModelState.AddModelError("", "Kullanıcı adı veya şifre hatalı");
+            return View();
+        }
+    }
+}
