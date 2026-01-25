1c75655888784cb1e6be35bc188b8e38683926c6
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-01-25 16:57:14 +0300

    Mesaj Hub'ı oluşturuldu.

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -116,5 +116,11 @@ namespace SignalRApi.Hubs
             await Clients.All.SendAsync("ReceiveMenuTableStatus",value);
         }
 
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+
+        }
+
     }
 }
