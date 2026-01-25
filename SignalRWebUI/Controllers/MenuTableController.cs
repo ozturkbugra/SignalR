306c3f5f45989ce05514f6b8afddc27f00ec476e
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-01-25 16:14:44 +0300

    Admin panelindeki menutable sınıfı CRUD işlemi yapıldı.

---
+++ SignalRWebUI/Controllers/MenuTableController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.MenuTableDtos;
+
+namespace SignalRWebUI.Controllers
+{
+    public class MenuTableController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MenuTableController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responeMessage = await client.GetAsync("https://localhost:7171/api/MenuTables");
+            if (responeMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responeMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultMenuTableDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateMenuTable()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateMenuTable(CreateMenuTableDto createMenuTableDto)
+        {
+            createMenuTableDto.Status = false;
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createMenuTableDto);
+            StringContent stringContent = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7171/api/MenuTables", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteMenuTable(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7171/api/MenuTables/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> UpdateMenuTable(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7171/api/MenuTables/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<GetMenuTableDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateMenuTable(UpdateMenuTableDto updateMenuTableDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateMenuTableDto);
+            StringContent stringContent = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7171/api/MenuTables", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ SignalRWebUI/Dtos/MenuTableDtos/ResultMenuTableDto.cs
@@ -0,0 +1,12 @@
+﻿using NuGet.ContentModel;
+
+namespace SignalRWebUI.Dtos.MenuTableDtos
+{
+    public class ResultMenuTableDto
+    {
+        public int MenuTableID { get; set; }
+        public string Name { get; set; }
+        public bool Status { get; set; }
+      
+    }
+}
