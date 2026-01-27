d63778c2d024c154cacd6652a142d29a174faedd
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-01-27 21:53:31 +0300

    Mail gönderme backend kodu yazıldı

---
+++ SignalRWebUI/Controllers/MailController.cs
@@ -0,0 +1,42 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using SignalRWebUI.Dtos.MailDtos;
+
+namespace SignalRWebUI.Controllers
+{
+    public class MailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CreateMailDto dto)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+
+            MailboxAddress mailboxAddress = new MailboxAddress("SignalR Rezervasyon","Mail Adresi");
+            mimeMessage.From.Add(mailboxAddress);
+
+            MailboxAddress mailboxAddressto = new MailboxAddress("User", dto.RecieverMail);
+            mimeMessage.To.Add(mailboxAddressto);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = dto.Body;
+            mimeMessage.Body= bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = dto.Subject;
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp:gmail.com",587,false);
+            client.Authenticate("mail adresi", "key");
+            
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+            
+            return RedirectToAction("Index","Category");
+        }
+    }
+}
