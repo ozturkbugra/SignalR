87037d4060ea9766591323c2443b468a714fff63
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-01-27 21:09:55 +0300

    Kullanıcı bilgi güncelleme işlemi yapıldı.

--- SignalRWebUI/Controllers/SettingController.cs
+++ SignalRWebUI/Controllers/SettingController.cs
@@ -26,5 +26,24 @@ namespace SignalRWebUI.Controllers
             userEditDto.Mail = value.Email;
             return View(userEditDto);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditDto dto)
+        {
+            if (dto.Password == dto.ConfirmPassword)
+            {
+                var user = await _userManager.FindByNameAsync(User.Identity.Name);
+                user.Name = dto.Name;
+                user.Surname = dto.Surname;
+                user.Email = dto.Mail;
+                user.UserName = dto.Username;
+                user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, dto.Password);
+                var result = await _userManager.UpdateAsync(user);
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+
+
     }
 }
