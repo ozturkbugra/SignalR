c73159bdcf4ceb2224d445a10d63db5431196f5a
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-01-27 21:03:03 +0300

    Veriler getirildi

---
+++ SignalRWebUI/Controllers/SettingController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.EntityLayer.Entities;
+using SignalRWebUI.Dtos.IdentityDtos;
+
+namespace SignalRWebUI.Controllers
+{
+    [Authorize]
+    public class SettingController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public SettingController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var value = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserEditDto userEditDto = new UserEditDto();
+            userEditDto.Surname = value.Surname;
+            userEditDto.Username = value.UserName;
+            userEditDto.Name = value.Name;
+            userEditDto.Mail = value.Email;
+            return View(userEditDto);
+        }
+    }
+}
