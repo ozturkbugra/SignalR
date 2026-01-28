258ec203233924131b0fa57083c03ef4f35b2af0
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-01-28 21:40:16 +0300

    Masa sayfası tasarımı yapıldı.

---
+++ SignalRWebUI/Controllers/CustomerTableController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.Controllers
+{
+    public class CustomerTableController : Controller
+    {
+        public IActionResult CustomerTableList()
+        {
+            return View();
+        }
+    }
+}
