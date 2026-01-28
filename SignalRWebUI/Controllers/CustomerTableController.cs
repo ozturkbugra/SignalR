7837e9123b812d587af61d78f638fd5f4c7c2d08
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-01-28 21:49:11 +0300

    masalar listlendi.

--- SignalRWebUI/Controllers/CustomerTableController.cs
+++ SignalRWebUI/Controllers/CustomerTableController.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.MenuTableDtos;
+using System.Net.Http;
 
 namespace SignalRWebUI.Controllers
 {
     public class CustomerTableController : Controller
     {
-        public IActionResult CustomerTableList()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CustomerTableController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> CustomerTableList()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responeMessage = await client.GetAsync("https://localhost:7171/api/MenuTables");
+            if (responeMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responeMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultMenuTableDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
